5fa477e855bc9c7b726cae93dee92870323b9115
Mehdi OUSFANE <64252905+mehdi-ousfane@users.noreply.github.com>
2020-11-23 10:46:52 +0100

    views and controllers

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error404');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.renderError);
 
 app.listen(3000);
