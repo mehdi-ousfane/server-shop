10dfdc109cf88694073dd54083fbf44bbef90a14
Mehdi OUSFANE <64252905+mehdi-ousfane@users.noreply.github.com>
2020-11-28 20:56:49 +0100

    some style and functions adj

--- models/product.js
+++ models/product.js
@@ -1,35 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathProduct = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (cb) => {
-    fs.readFile(pathProduct, (err, fileContent) => {
-        if (err) {
-            return cb([]);
-        }
-        cb(JSON.parse(fileContent));
-    });
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
 };
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
-        this.title = title;
-        this.imageUrl = imageUrl,
-        this.description = description,
-        this.price = price
-    }
-    save() {
-        getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFileSync(pathProduct, JSON.stringify(products));
-        });
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    static fetchAll(cb) {
-        getProductsFromFile(cb)
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 };
